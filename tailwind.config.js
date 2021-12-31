db178ac46b76e0315ffd29e4a7e63f1846f12d8b
Karol Proksa <karolproksa@gmail.com>
2021-12-31 16:02:41 +0100

    A starter for porting landing page to React

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['secondaryFont', 'Helvetica', 'Roboto', 'Lato', 'Arial'],
+      'display': ['mainFont', 'Helvetica', 'Roboto', 'Lato', 'Arial'],
+    }
+  },
+  plugins: [],
+}
